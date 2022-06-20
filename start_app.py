86441ff1ad9b921f49dfe3cc2620f60460a4d12a
Ashish Ojha <vickyojha2@yahoo.com>
2022-06-21 01:45:24 +0530

    Create start_app.py

---
+++ start_app.py
@@ -0,0 +1,9 @@
+
+import os
+
+# Change working dir, to the root path of the project
+ch = os.chdir("./path/to/project/root")
+
+# Run your main/bootstrap file
+import app as a
+# Execute app or the app may have gotten executed anyway
